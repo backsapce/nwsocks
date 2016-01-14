069f2ea076bf0830abeef3bc23efb95053763687
黄垚 <huangyao@huangyaos-MacBook-Pro.local>
2016-01-14 11:44:07 +0800

    fix config

--- tcp/tcp.js
+++ tcp/tcp.js
@@ -5,9 +5,9 @@ var Encryptor = require("../encrop/encrypt").Encryptor;
 var debug = require('debug')('tcp');
 debug('tcp');
 var server = net.createServer(function(socket) {
-	let encryptor = new Encryptor(global.passwd, global.method)
+	let encryptor = new Encryptor(global.config.passwd, global.config.method)
 	let stream = new tcpStream(socket, encryptor);
-	let remote = net.connect(global.port, global.host);
+	let remote = net.connect(global.config.port, global.config.host);
 	remote.on('connect', function() {
 		debug('connect');
 		socket.pipe(stream).pipe(remote).pipe(encryptor).pipe(socket);
